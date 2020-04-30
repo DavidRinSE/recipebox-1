9ec889f22d40c0f1be47261a888ef179fe5cb0f7
luisfff29 <luisfff29@gmail.com>
2020-04-30 18:43:12 -0500

    create models and migrate

--- recipe_app/models.py
+++ recipe_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+    bio = models.TextField()
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=30)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    description = models.TextField()
+    time_required = models.CharField(max_length=30)
+    instructions = models.TextField()
