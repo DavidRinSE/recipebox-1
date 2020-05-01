ddc4a8f4b1af023408dd6b43d5dba57f7b6586b1
luisfff29 <luisfff29@gmail.com>
2020-05-01 12:27:09 -0500

    add url requests

--- recipe_app/urls.py
+++ recipe_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from recipe_app import views
 
 urlpatterns = [
-    path('', views.index),
-    path('recipes/<int:id>/', views.recipes),
-    path('authors/<int:id>/', views.authors),
+    path('', views.index, name="home"),
+    path('recipes/<int:id>/', views.recipes, name="recipes"),
+    path('authors/<int:id>/', views.authors, name="authors"),
 ]
