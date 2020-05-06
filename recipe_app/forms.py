a4e86c71307b9badebefda0263bff72876acd184
luisfff29 <luisfff29@gmail.com>
2020-05-06 08:30:07 -0500

    add bootstrap/css into addrecipe

--- recipe_app/forms.py
+++ recipe_app/forms.py
@@ -3,30 +3,35 @@ from recipe_app.models import Author
 
 
 class AddAuthorForm(forms.ModelForm):
-    name = forms.CharField(widget=forms.TextInput(
-        attrs={
-            'class': 'form-control',
-            'placeholder': 'Name'
-        }
+    name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Name'
+    }
     ))
 
-    bio = forms.CharField(widget=forms.Textarea(
-        attrs={
-            'class': 'form-control',
-            'placeholder': 'Short bio...'
-        }
+    bio = forms.CharField(widget=forms.Textarea(attrs={
+        'class': 'form-control',
+        'placeholder': 'Short bio...'}
     ))
 
     class Meta:
         model = Author
-        fields = [
-            'name', 'bio'
-        ]
+        fields = ['name', 'bio']
 
 
 class AddRecipeForm(forms.Form):
-    title = forms.CharField(max_length=30)
+    title = forms.CharField(max_length=30, widget=forms.TextInput(attrs={
+        'class': 'w-50'
+    }))
     author = forms.ModelChoiceField(queryset=Author.objects.all())
-    description = forms.CharField(widget=forms.Textarea)
-    time_required = forms.CharField(max_length=30)
-    instructions = forms.CharField(widget=forms.Textarea)
+    description = forms.CharField(
+        widget=forms.Textarea(attrs={
+            'placeholder': 'Enter description here...'
+        }))
+    time_required = forms.CharField(
+        max_length=30, widget=forms.TextInput(attrs={
+            'placeholder': 'Ex: One hour'
+        }))
+    instructions = forms.CharField(widget=forms.Textarea(attrs={
+        'placeholder': 'Enter Instructions here...'
+    }))
